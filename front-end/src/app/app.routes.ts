8a5fb3ca29dd82d3d46b991fc411e226da68170b
Gabriel Nonnemacher <nonnemacher99gabriel@gmail.com>
2026-05-15 09:24:29 -0300

    feat: add news page and tests

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -2,9 +2,21 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home/home').then((m) => m.HomePage) },
-  { path: 'history', loadComponent: () => import('./pages/history/history').then((m) => m.HistoryPage) },
-  { path: 'alerts', loadComponent: () => import('./pages/alerts/alerts').then((m) => m.AlertsPage) },
-  { path: 'news', loadComponent: () => import('./pages/news/news').then((m) => m.NewsPage) },
-  { path: 'useful-links', loadComponent: () => import('./pages/useful-links/useful-links').then((m) => m.UsefulLinksPage) },
+  {
+    path: 'history',
+    loadComponent: () => import('./pages/history/history').then((m) => m.HistoryPage),
+  },
+  {
+    path: 'alerts',
+    loadComponent: () => import('./pages/alerts/alerts').then((m) => m.AlertsPage),
+  },
+  {
+    path: 'news',
+    loadComponent: () => import('./pages/news/news').then((m) => m.NewsPage),
+  },
+  {
+    path: 'useful-links',
+    loadComponent: () => import('./pages/useful-links/useful-links').then((m) => m.UsefulLinksPage),
+  },
   { path: 'about', loadComponent: () => import('./pages/about/about').then((m) => m.AboutPage) },
 ];
---
+++ front-end/src/app/shared/constants/news.constants.ts
@@ -0,0 +1,61 @@
+export const LIST_NEWS = [
+  {
+    id: 'defesa-civil-promove-capacitacao-em-comunicacao-de-risco-no-primeiro-dia-de-programacao-em-alusao-aos-eventos-extremos-de-2024',
+    title:
+      'Defesa Civil promove Capacitação em Comunicação de Risco no primeiro dia de programação em alusão aos eventos extremos de 2024',
+    description:
+      'Nesta segunda-feira (4) teve início, na Universidade Franciscana (UFN), em Santa Maria, a semana de eventos em memória aos dois anos das enchentes que atingiram o Rio Grande do Sul em 2024. Neste primeiro dia, as atividades foram conduzidas pela Defesa Civil estadual, com a realização da 1ª Capacitação em Comunicação de Risco para Municípios.',
+    date: '05/05/2026 às 22h56min',
+    link: 'https://www.defesacivil.rs.gov.br/defesa-civil-promove-capacitacao-em-comunicacao-de-risco-no-primeiro-dia-de-programacao-em-alusao-aos-eventos-extremos-de-2024',
+    image: 'https://www.defesacivil.rs.gov.br/upload/recortes/202605/05224817_122457_GDO.jpeg',
+  },
+  {
+    id: 'assinado-contrato-para-construcao-do-centro-estadual-de-gestao-integrada-de-riscos-e-desastres',
+    title: 'Assinado contrato para construção do Centro Estadual de Gestão Integrada de Riscos e Desastres',
+    description:
+      'O governador do Estado, Eduardo Leite, assinou nesta quarta-feira (18/3), a contratação de projeto e execução da obra para a construção do Centro Estadual de Gestão Integrada de Riscos e Desastres (Cegird), estrutura responsável por coordenar as ações de Proteção e Defesa Civil no âmbito estadual.',
+    date: '18/03/2026 às 17h49min',
+    link: 'https://www.defesacivil.rs.gov.br/assinado-contrato-para-construcao-do-centro-estadual-de-gestao-integrada-de-riscos-e-desastres',
+    image: 'https://www.defesacivil.rs.gov.br/upload/recortes/202603/18175815_119234_GDO.jpg',
+  },
+  {
+    id: 'defesa-civil-estadual-participa-de-simulados-realiza-palestras-e-cumpre-agenda-de-visitas-tecnicas',
+    title: 'Defesa Civil Estadual participa de simulados, realiza palestras e cumpre agenda de visitas técnicas',
+    description:
+      'Ao longo desta semana, a Casa Militar, por meio da Subchefia de Proteção e Defesa Civil, deu continuidade às ações em diferentes regiões do Estado. As coordenadorias regionais estiveram envolvidas em visita técnicas, fortalecendo a articulação com os municípios.',
+    date: '17/10/2025 às 11h54min',
+    link: 'https://www.defesacivil.rs.gov.br/defesa-civil-estadual-participa-de-simulados-realiza-palestras-e-cumpre-agenda-de-visitas-tecnicas',
+    image: 'https://www.defesacivil.rs.gov.br/upload/recortes/202510/17115335_105240_GDO.jpeg',
+  },
+  {
+    id: 'defesa-civil-participa-de-seminario-do-mprs-sobre-prevencao-e-resposta-a-desastres-com-foco-em-planos-de-contingencia',
+    title:
+      'Defesa Civil participa de seminário do MPRS sobre prevenção e resposta a desastres com foco em planos de contingência',
+    description:
+      'A Defesa Civil do Estado esteve em Uruguaiana nesta terça-feira, 14 de outubro, para participar do Seminário “Prevenção, Preparação e Resposta a Desastres: Sensibilização e Qualificação dos Planos de Contingência Municipais – O Olhar do MP”, promovido pelo Gabinete de Estudos Climáticos (GabClima) do Ministério Público do Rio Grande do Sul (MPRS). O encontro ocorreu no SEST SENAT e reuniu especialistas para discutir estratégias de enfrentamento aos eventos climáticos extremos.',
+    date: '15/10/2025 às 07h36min',
+    link: 'https://www.defesacivil.rs.gov.br/defesa-civil-participa-de-seminario-do-mprs-sobre-prevencao-e-resposta-a-desastres-com-foco-em-planos-de-contingencia',
+    image: 'https://www.defesacivil.rs.gov.br/upload/recortes/202510/15072914_104961_GDO.jpeg',
+  },
+
+  {
+    id: 'defesa-civil-do-rs-participa-de-evento-sobre-rios-em-porto-alegre',
+    title: 'Defesa Civil do RS participa de evento sobre rios em Porto Alegre',
+    description:
+      'A Defesa Civil do Rio Grande do Sul participou, nesta quinta-feira (28/8), da iniciativa “Navegar é preciso: nossos rios em debate”, que ocorre na Câmara de Vereadores de Porto Alegre. O evento reuniu um painel de especialistas para discutir soluções e medidas preventivas para novas inundações no estado.',
+    date: '29/08/2025 às 16h15min',
+    link: 'https://www.defesacivil.rs.gov.br/defesa-civil-do-rs-participa-de-evento-sobre-rios-em-porto-alegre',
+    image: 'https://www.defesacivil.rs.gov.br/upload/recortes/202508/28135712_99965_GDO.jpg',
+  },
+
+  {
+    id: 'defesa-civil-do-estado',
+    title:
+      'Defesa Civil do Estado participa do Seminário Internacional sobre Educação, Mudanças Climáticas e Resiliência',
+    description:
+      'A Casa Militar – Subchefia de Proteção e Defesa Civil do Rio Grande do Sul, participou, nos dias 5 e 6 de agosto, do Seminário Internacional sobre Educação, Mudanças Climáticas e Resiliência, realizado pela Secretaria da Educação (Seduc), com a cooperação internacional do Banco Mundial e apoio técnico do movimento Todos pela Educação.',
+    date: ' 06/08/2025 às 15h39min',
+    link: 'https://www.defesacivil.rs.gov.br/defesa-civil-do-estado',
+    image: 'https://www.defesacivil.rs.gov.br/upload/recortes/202508/06153722_97187_GDO.png',
+  },
+];
