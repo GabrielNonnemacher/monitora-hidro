f7a6012f0022b683163033ff4778f3b425498151
Gabriel Nonnemacher <nonnemacher99gabriel@gmail.com>
2026-05-15 11:00:19 -0300

    refactor: adjust dashboard request/response and remove alerts page

--- back-end/src/modules/measurements/measurements.service.ts
+++ back-end/src/modules/measurements/measurements.service.ts
@@ -1,9 +1,10 @@
 import { Measurement } from '@monitora-hidro/schemas';
-import { ApiResponse } from '@monitora-hidro/shared';
+import { ApiResponse, FilterChart } from '@monitora-hidro/shared';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateMeasurementDto } from './dto/create-measurement.dto';
+import { ChartMeasurementDataDto } from './dto/response-chart-measurement.dto';
 import { MeasurementDto } from './dto/response-measurement.dto';
 
 @Injectable()
@@ -30,18 +31,80 @@ export class MeasurementsService {
     };
   }
 
-  public async findAllMeasurements(
+  public async getChartData(
     locationId: string,
-  ): Promise<ApiResponse<MeasurementDto[] | null>> {
-    const measurements = await this.measurementModel?.find({ locationId });
-    const hasMeasurements = measurements?.length > 0;
+    filter: string,
+  ): Promise<ApiResponse<ChartMeasurementDataDto[] | null>> {
+    const now = new Date();
+    let startDate = new Date();
+    let groupId: any;
+    let limit = 0;
+    let error = false;
+
+    switch (filter) {
+      case FilterChart.years:
+        startDate.setFullYear(now.getFullYear() - 5);
+        groupId = { $year: '$date' };
+        limit = 5;
+        break;
+      case FilterChart.months:
+        startDate.setMonth(now.getMonth() - 12);
+        groupId = { $month: '$date' };
+        limit = 12;
+        break;
+      case FilterChart.weeks:
+        startDate.setDate(now.getDate() - 35);
+        groupId = { $isoWeek: '$date' };
+        limit = 5;
+        break;
+      case FilterChart.days:
+        startDate.setDate(now.getDate() - 12);
+        groupId = { $dayOfMonth: '$date' };
+        limit = 12;
+        break;
+      case FilterChart.today:
+        startDate.setHours(now.getHours() - 5);
+        groupId = { $hour: '$date' };
+        limit = 5;
+        break;
+      default:
+        error = true;
+        break;
+    }
+
+    const result = await this.measurementModel.aggregate([
+      {
+        $match: {
+          locationId,
+          date: {
+            $gte: startDate,
+            $lte: now,
+          },
+        },
+      },
+      {
+        $group: {
+          _id: groupId,
+          measurement: {
+            $avg: '$measurement',
+          },
+        },
+      },
+      { $sort: { _id: 1 } },
+      { $limit: limit },
+      {
+        $project: {
+          _id: 0,
+          measurement: { $round: ['$measurement', 2] },
+          x: { $toString: '$_id' },
+        },
+      },
+    ]);
 
     return {
-      success: true,
-      data: hasMeasurements ? measurements : null,
-      message: hasMeasurements
-        ? undefined
-        : 'Nenhuma medição encontrada para o local especificado',
+      success: error,
+      data: result ?? null,
+      message: error ? 'Erro no filtro' : undefined,
     };
   }
 
---
+++ back-end/src/shared/types/filter-chart.ts
@@ -0,0 +1,7 @@
+export enum FilterChart {
+  years = 'years',
+  months = 'months',
+  weeks = 'weeks',
+  days = 'days',
+  today = 'today',
+}
