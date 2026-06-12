2f0f4dfe1e9a62656887740eff41d2875773e87b
Gabriel Nonnemacher <87139289+GabrielNonnemacher@users.noreply.github.com>
2026-06-12 00:10:27 -0300

    fix: Change default time interval for measurements to 30 seconds

--- front-end/src/app/pages/home/home.ts
+++ front-end/src/app/pages/home/home.ts
@@ -116,7 +116,7 @@ export class HomePage implements OnInit {
     }
   }
 
-  private getLastestMeasurement(locationId: string, timeInterval: number = 120000): void {
+  private getLastestMeasurement(locationId: string, timeInterval: number = 30000): void {
     interval(timeInterval)
       .pipe(
         switchMap(() =>
